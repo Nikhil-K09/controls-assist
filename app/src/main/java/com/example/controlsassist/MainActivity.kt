98979d2c76adae742d162a7e4f55cf4831961f78
Nikhil-K09 <kalavaguntanikhil@gmail.com>
2025-08-07 17:01:15 +0530

    partial layout

--- app/src/main/java/com/example/controlsassist/MainActivity.kt
+++ app/src/main/java/com/example/controlsassist/MainActivity.kt
@@ -1,20 +1,77 @@
 package com.example.controlsassist
 
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.GravityCompat
+import com.example.controlsassist.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPref: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Apply saved theme before setting content view
+        sharedPref = getSharedPreferences("ControlPrefs", MODE_PRIVATE)
+        val isDarkMode = sharedPref.getBoolean("dark_mode", false)
+        AppCompatDelegate.setDefaultNightMode(
+            if (isDarkMode) AppCompatDelegate.MODE_NIGHT_YES
+            else AppCompatDelegate.MODE_NIGHT_NO
+        )
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupToolbarAndDrawer()
+        setupDrawerMenu()
+    }
+
+    private fun setupToolbarAndDrawer() {
+        // Set up custom menu icon click
+        binding.menuIcon.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+
+    private fun setupDrawerMenu() {
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_toggle_dark_mode -> {
+                    toggleTheme()
+                    true
+                }
+
+                // Add other menu actions here as needed
+
+                else -> false
+            }
+        }
+    }
+
+    private fun toggleTheme() {
+        val isDarkMode = sharedPref.getBoolean("dark_mode", false)
+        sharedPref.edit().putBoolean("dark_mode", !isDarkMode).apply()
+
+        AppCompatDelegate.setDefaultNightMode(
+            if (!isDarkMode)
+                AppCompatDelegate.MODE_NIGHT_YES
+            else
+                AppCompatDelegate.MODE_NIGHT_NO
+        )
+    }
+
+    override fun onBackPressed() {
+        // Close drawer if open
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 }
